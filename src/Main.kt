18866404e9aee76f4497c833af8d841ee449743c
NIDHI <nidhitrentiya.25@gmail.com>
2026-06-22 14:31:00 +0530

    pr11

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,6 @@ fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, wold!")
+    println("Hello, world!")
     println()
 }
---
+++ src/pr11.kt
@@ -0,0 +1,27 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    var i=1
+    var j:Int=2
+    var k:Float=1.5f
+    var l:Char='n'
+    var m:String="HMS"
+    var n:Double=96.36
+    var a:Long= 3452784682
+    var b:Short=-2
+    var c:Byte=127
+    var d:Boolean=false
+    println("j="+j)
+    println("i="+i)
+    println("k="+k)
+    println("l="+l)
+    println("m="+m)
+    println("n="+n)
+    println("a="+a)
+    println("b="+b)
+    println("c="+c)
+    println("d="+d)
+    println("Hello, world!!!")
+}
