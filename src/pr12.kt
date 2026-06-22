628b6e6d5a729f7d05793ca35e55cab02fd7ef26
NIDHI <nidhitrentiya.25@gmail.com>
2026-06-22 15:02:11 +0530

    pr12

--- src/pr12.kt
+++ src/pr12.kt
@@ -1,7 +1,12 @@
 fun main() {
     val intValue: Int = 42
     val doubleValue: Double = intValue.toDouble()
+    val validString = "123"
+    val integer: Int = validString.toInt()
+    println(integer)
+    println(doubleValue)
 
-    println(doubleValue) 
+
+
 
 }
