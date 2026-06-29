1f4cd2aa76898f726e4d80440fdb0eb3e6411e15
NIDHI <nidhitrentiya.25@gmail.com>
2026-06-29 20:50:29 +0530

    pr16

--- src/pr16.kt
+++ src/pr16.kt
@@ -1,8 +1,24 @@
 fun main(){
-    var n=2
-    var m=3
-
+    var n=111
+    var m=2222
+    println("Addition of 111,2222 is ${n+m}")
+    println("subtraction of 111,2222 is ${n-m}")
+    println("Multiplication of 111,2222 is ${n*m}")
+    println("Division of 2222,111 is ${m/n}")
 }
 fun add(n:Int,m:Int ):Int{
-return(n+m)
+    var result=n+m
+    return result
+}
+fun sub(n:Int,m:Int):Int{
+    var result=n-m
+    return result
+}
+fun multiply(n:Int,m:Int):Int{
+    var result=n*m
+    return result
+}
+fun div(m:Int,n:Int):Int{
+    var result=m/n
+    return result
 }
